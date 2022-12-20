0862b6fcced8ee11fea0122080174b8da364a0c9
DoctorPok42 <remi.mazat@epitech.eu>
2022-12-20 18:46:51 +0100

    first commit

---
+++ components/Header/index.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+
+}
+
+export default Header
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { Header } from "../components"
+
+export default function Home() {
+  return (
+    <>
+
+    </>
+  )
+}
