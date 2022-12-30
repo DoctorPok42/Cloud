e980c0e3ea7426439e04feb490752f8b5b486a67
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2022-12-30 19:49:57 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head"
 import { Header } from "../components"
 
 export default function Home() {
   return (
     <>
-
+      <Head>
+        <title>Cloud</title>
+      </Head>
+      <Header title="Cloud" />
     </>
   )
 }
