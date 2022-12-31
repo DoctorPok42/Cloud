7c90df112f5b40479f89d9dd5309c64e1209cde9
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2022-12-31 16:35:02 +0100

    component

---
+++ components/SideBarButton/index.tsx
@@ -0,0 +1,25 @@
+import { Part } from '../../types';
+import styles from './style.module.scss';
+
+interface SidebarButtonProps {
+    name: string;
+    page: Part;
+    handleChangePart: (part: Part) => void;
+    children: React.ReactNode;
+}
+
+const SidebarButton = ({ name, page, handleChangePart, children }: SidebarButtonProps) => {
+  return (
+    <a
+      className={styles.button}
+      style={{
+        backgroundColor: page === name ? "var(--blue)" : undefined,
+      }}
+      onClick={() => handleChangePart(page)}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default SidebarButton;
--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,14 +1,41 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHardDrive,
+  faUserGroup
+} from "@fortawesome/free-solid-svg-icons";
+
+import { Part } from "../../types/index";
 import styles from './style.module.scss';
+import SidebarButton from '../SideBarButton';
 
 interface SidebarProps {
-    title: string;
+    page: Part;
+    setPage: (page: Part) => void;
 }
 
-const Sidebar = ({title}: SidebarProps) => {
+const Sidebar = ({page, setPage}: SidebarProps) => {
+
+    const handleChangePart = (part: Part) => {
+        if (part === "my_drive") {
+            setPage("shared_drive")
+            window.location.href = "/shared";
+        } else {
+            setPage("my_drive")
+            window.location.href = "/";
+        }
+    }
+
     return (
-        <div className={styles.bandeau}>
-            <img src="/favicon.ico" alt="logo" />
-            <h2>{title}</h2>
+        <div className={styles.sidebar}>
+            <SidebarButton name="my_drive" page={page} handleChangePart={handleChangePart}>
+                <FontAwesomeIcon icon={faHardDrive} className={styles.icon} />
+                <h2>Mon drive</h2>
+            </SidebarButton>
+
+            <SidebarButton name="shared_drive" page={page} handleChangePart={handleChangePart}>
+                <FontAwesomeIcon icon={faUserGroup} className={styles.icon} />
+                <h2>Drive partagé</h2>
+            </SidebarButton>
         </div>
     )
 }
