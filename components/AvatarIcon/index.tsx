c3a1e41a206e4710c2d7be8fe0337c2db2857ee4
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2023-02-03 12:25:14 +0100

    Create user icon menu

---
+++ components/AvatarIcon/index.tsx
@@ -0,0 +1,104 @@
+import { IconButton, ListItemIcon, Menu, MenuItem } from "@mui/material";
+import Image from "next/image";
+import React, { useState } from "react";
+import styles from "./style.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+
+interface AvatarProps {
+  username: string;
+}
+
+const AvatarIcon = ({ username }: AvatarProps) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    fetch(`/api/logout`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      if (res.status === 200) {
+        window.location.href = "/";
+      }
+    });
+  };
+
+  return (
+    <div className={styles.avatar}>
+      <IconButton
+        onClick={handleClick}
+        aria-controls={open ? "account-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+      >
+        <img
+          alt={username}
+          src={`https://avatars.dicebear.com/api/initials/${username}.png?radius=50`}
+        />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            mt: 1.3,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 17,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem onClick={handleLogout}>
+          <FontAwesomeIcon
+            icon={faArrowRightFromBracket}
+            style={{ marginRight: "10px", color: "var(--red)" }}
+          />
+          <h2
+            style={{
+              color: "var(--red)",
+              fontSize: "1rem",
+              fontWeight: "bold",
+            }}
+          >
+            Logout
+          </h2>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+export default AvatarIcon;
