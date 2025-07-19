4ad3a5f520fcc7988cfb4f4a5334dccc19aa5c40
DoctorPok <pokdoctor@gmail.com>
2025-07-19 12:40:11 +0200

    Feat -> header: new design

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { AvatarIcon, BreadCrumbs } from "../../components/";
 
 import styles from "./style.module.scss";
@@ -6,16 +8,31 @@ interface HeaderProps {
   cookies: string;
   path: string;
   setPath: (path: string) => void;
+  isReduced: boolean;
+  setIsReduced: (isReduced: boolean) => void;
 }
 
 const Header = ({
   cookies,
   path,
   setPath,
+  isReduced,
+  setIsReduced,
 }: HeaderProps) => {
   const username = cookies.split(";").find((item) => item.trim().startsWith("username="))?.split("=")[1] as string;
   return (
     <div className={styles.bandeau}>
+      <FontAwesomeIcon
+        icon={faCircleChevronLeft}
+        className={styles.arrow}
+        width={21.5}
+        height={21.5}
+        onClick={() => setIsReduced(!isReduced)}
+        style={{
+          transform: isReduced ? "rotate(-180deg)" : "rotate(0deg)",
+        }}
+      />
+
       <BreadCrumbs
         newPath={path}
         setNewPath={setPath}
